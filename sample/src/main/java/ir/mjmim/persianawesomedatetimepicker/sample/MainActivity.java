edfbb41a1bc485cc806bea20ad5ba54a71518f5a
MiM <mortezamim@gmail.com>
2019-07-19 19:11:51 +0430

    CleanUp

--- sample/src/main/java/ir/mjmim/persianawesomedatetimepicker/sample/MainActivity.java
+++ sample/src/main/java/ir/mjmim/persianawesomedatetimepicker/sample/MainActivity.java
@@ -2,140 +2,108 @@ package ir.mjmim.persianawesomedatetimepicker.sample;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
-
-import ir.mjmim.persianawesomedatetimepicker.date.DatePickerDialog;
-import ir.mjmim.persianawesomedatetimepicker.multidate.MultiDatePickerDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import com.mohamadamin.persianmaterialdatetimepicker.sample.R;
+import ir.mjmim.persianawesomedatetimepicker.date.DatePickerDialog;
+import ir.mjmim.persianawesomedatetimepicker.date.DatePickerDialog.OnDateSetListener;
 import ir.mjmim.persianawesomedatetimepicker.time.RadialPickerLayout;
 import ir.mjmim.persianawesomedatetimepicker.time.TimePickerDialog;
-import ir.mjmim.persianawesomedatetimepicker.utils.PersianCalendar;
-
-import ir.mjmim.persianawesomedatetimepicker.date.DatePickerDialog.OnDateSetListener;
 import ir.mjmim.persianawesomedatetimepicker.time.TimePickerDialog.OnTimeSetListener;
-import java.util.ArrayList;
-import java.util.Calendar;
+import ir.mjmim.persianawesomedatetimepicker.utils.PersianCalendar;
 
 public class MainActivity extends AppCompatActivity implements
     OnTimeSetListener,
     OnDateSetListener,
-    MultiDatePickerDialog.OnDateSetListener,
-        View.OnClickListener {
+    View.OnClickListener {
 
-    private static final String TIMEPICKER = "TimePickerDialog",
-            DATEPICKER = "DatePickerDialog", MULTIDATEPICKER = "MultiDatePickerDialog";
+  private static final String TIMEPICKER = "TimePickerDialog",
+      DATEPICKER = "DatePickerDialog";
 
-    private CheckBox mode24Hours, modeDarkTime, modeDarkDate;
-    private TextView timeTextView, dateTextView, multiDateTextView;
-    private Button timeButton, dateButton, multiDataButton;
+  private CheckBox mode24Hours, modeDarkTime, modeDarkDate;
+  private TextView timeTextView, dateTextView;
+  private Button timeButton, dateButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initializeViews();
-        handleClicks();
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    initializeViews();
+    handleClicks();
+  }
+
+  private void initializeViews() {
+    timeTextView = findViewById(R.id.time_textview);
+    dateTextView = findViewById(R.id.date_textview);
+    timeButton = findViewById(R.id.time_button);
+    dateButton = findViewById(R.id.date_button);
+    mode24Hours = findViewById(R.id.mode_24_hours);
+    modeDarkTime = findViewById(R.id.mode_dark_time);
+    modeDarkDate = findViewById(R.id.mode_dark_date);
+  }
+
+  private void handleClicks() {
+    timeButton.setOnClickListener(this);
+    dateButton.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View view) {
+    String fontName = "DroidNaskh-Regular";
+    switch (view.getId()) {
+      case R.id.time_button: {
+        PersianCalendar now = new PersianCalendar();
+        TimePickerDialog tpd = TimePickerDialog.newInstance(
+            MainActivity.this,
+            now.get(PersianCalendar.HOUR_OF_DAY),
+            now.get(PersianCalendar.MINUTE),
+            mode24Hours.isChecked()
+        );
+        tpd.setThemeDark(modeDarkTime.isChecked());
+        tpd.setTypeface(fontName);
+        tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
+          @Override
+          public void onCancel(DialogInterface dialogInterface) {
+            Log.d(TIMEPICKER, "Dialog was cancelled");
+          }
+        });
+        tpd.show(getFragmentManager(), TIMEPICKER);
+        break;
+      }
+      case R.id.date_button: {
+        PersianCalendar now = new PersianCalendar();
+        DatePickerDialog dpd = DatePickerDialog.newInstance(
+            MainActivity.this,
+            now.getPersianYear(),
+            now.getPersianMonth(),
+            now.getPersianDay()
+        );
+        dpd.setThemeDark(modeDarkDate.isChecked());
+        dpd.setTypeface(fontName);
+        dpd.show(getFragmentManager(), DATEPICKER);
+        break;
+      }
+      default:
+        break;
     }
+  }
 
-    private void initializeViews() {
-        timeTextView = findViewById(R.id.time_textview);
-        dateTextView = findViewById(R.id.date_textview);
-        multiDateTextView = findViewById(R.id.multi_date_textview);
-        timeButton = findViewById(R.id.time_button);
-        dateButton = findViewById(R.id.date_button);
-        multiDataButton = findViewById(R.id.multi_date_button);
-        mode24Hours = findViewById(R.id.mode_24_hours);
-        modeDarkTime = findViewById(R.id.mode_dark_time);
-        modeDarkDate = findViewById(R.id.mode_dark_date);
-    }
+  @Override
+  public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+    String hourString = hourOfDay < 10 ? "0" + hourOfDay : "" + hourOfDay;
+    String minuteString = minute < 10 ? "0" + minute : "" + minute;
+    String time = "You picked the following time: " + hourString + ":" + minuteString;
+    timeTextView.setText(time);
+  }
 
-    private void handleClicks() {
-        timeButton.setOnClickListener(this);
-        dateButton.setOnClickListener(this);
-        multiDataButton.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        String fontName = "DroidNaskh-Regular";
-        switch (view.getId()) {
-            case R.id.time_button: {
-                PersianCalendar now = new PersianCalendar();
-                TimePickerDialog tpd = TimePickerDialog.newInstance(
-                        MainActivity.this,
-                        now.get(PersianCalendar.HOUR_OF_DAY),
-                        now.get(PersianCalendar.MINUTE),
-                        mode24Hours.isChecked()
-                );
-                tpd.setThemeDark(modeDarkTime.isChecked());
-                tpd.setTypeface(fontName);
-                tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                    @Override
-                    public void onCancel(DialogInterface dialogInterface) {
-                        Log.d(TIMEPICKER, "Dialog was cancelled");
-                    }
-                });
-                tpd.show(getFragmentManager(), TIMEPICKER);
-                break;
-            }
-            case R.id.date_button: {
-                PersianCalendar now = new PersianCalendar();
-                DatePickerDialog dpd = DatePickerDialog.newInstance(
-                        MainActivity.this,
-                        now.getPersianYear(),
-                        now.getPersianMonth(),
-                        now.getPersianDay()
-                );
-                dpd.setThemeDark(modeDarkDate.isChecked());
-                dpd.setTypeface(fontName);
-                dpd.show(getFragmentManager(), DATEPICKER);
-                break;
-            }
-            case R.id.multi_date_button:
-                MultiDatePickerDialog mdpd = MultiDatePickerDialog.newInstance(MainActivity.this, null);
-                PersianCalendar[] pc = new PersianCalendar[30];
-                for (int i = 0; i < pc.length; i++) {
-                    pc[i] = new PersianCalendar();
-                    pc[i].add(Calendar.DAY_OF_YEAR, i);
-                }
-                mdpd.setMinDate(pc[0]);
-                mdpd.setMaxDate(pc[29]);
-                //mdpd.setSelectableDays(pc);
-                mdpd.setThemeDark(modeDarkDate.isChecked());
-                mdpd.setTypeface(fontName);
-                mdpd.show(getFragmentManager(), MULTIDATEPICKER);
-                break;
-            default:
-                break;
-        }
-    }
-
-    @Override
-    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
-        String hourString = hourOfDay < 10 ? "0" + hourOfDay : "" + hourOfDay;
-        String minuteString = minute < 10 ? "0" + minute : "" + minute;
-        String time = "You picked the following time: " + hourString + ":" + minuteString;
-        timeTextView.setText(time);
-    }
-
-    @Override
-    public void onDateSet(MultiDatePickerDialog view, ArrayList<PersianCalendar> selectedDays) {
-        StringBuilder date = new StringBuilder("You picked the following dates:\n\t");
-        for (PersianCalendar calendar : selectedDays) {
-            date.append(calendar.getPersianDay()).append("/").append(calendar.getPersianMonth() + 1).append("/").append(calendar.getPersianYear()).append("\n\t");
-        }
-        multiDateTextView.setText(date.toString());
-    }
-
-    @Override
-    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
-        // Note: monthOfYear is 0-indexed
-        String date = "You picked the following date: " + dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
-        dateTextView.setText(date);
-    }
+  @Override
+  public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+    // Note: monthOfYear is 0-indexed
+    String date = "You picked the following date: " + dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+    dateTextView.setText(date);
+  }
 }
